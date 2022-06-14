e8740b628740d4091967247c0216b818b9c32fa6
KindMaple <angelattack9759@gmail.com>
2022-06-14 23:56:33 +0900

    軽微な修正

--- bin/scripts/rustup.sh
+++ bin/scripts/rustup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)"
+RUSTUP_INIT_SKIP_PATH_CHECK=yes bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)"
